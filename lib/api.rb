b24ac7a621f418141a0a24a02e7551300fec32b0
Dmitriy Sorochenkov <podotkos@gmail.com>
2020-04-01 12:00:38 +0300

    Remove BaseWebApi, use self properly

--- lib/api.rb
+++ lib/api.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 require 'rack'
-require 'base'
 
 # web api
 class Api
-  include BaseWebApi
+  def self.call(env)
+    new(env).response.finish
+  end
+
+  def initialize(env)
+    @request = Rack::Request.new(env)
+  end
 
   def response
     case @request.path
-    when '/' then Rack::Response.new('test!!!')
+    when '/' then Rack::Response.new('test!')
     else Rack::Response.new('Not Found', 404)
     end
   end
