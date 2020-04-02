41a0491b414a2f937c37d0e62c0719872bec0466
Dmitriy Sorochenkov <podotkos@gmail.com>
2020-04-02 17:32:52 +0300

    Set not null constraint

--- db/migrations/3_user_project_m2m_table.rb
+++ db/migrations/3_user_project_m2m_table.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require 'sequel'
-require_relative '../../lib/models'
 
 Sequel.migration do
   up do
     create_table(:user_project_m2m) do
       primary_key :id, type: :Bignum
-      foreign_key :user_id, :user_account
-      foreign_key :project_id, :project
+      foreign_key :user_id, :user_account, null: false
+      foreign_key :project_id, :project, null: false
 
       index %i[user_id project_id]
     end
